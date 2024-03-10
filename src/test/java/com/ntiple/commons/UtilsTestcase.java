c7f646e9677c3dd763954508fd462fd857afb557
coder <coder@email.com>
2024-03-10 20:39:14 +0900

    테스트케이스 안되는 현상 수정

--- src/test/java/com/ntiple/commons/UtilsTestcase.java
+++ src/test/java/com/ntiple/commons/UtilsTestcase.java
@@ -1,9 +1,17 @@
+/**
+ * @File        : UtilsTestcase.java
+ * @Version     : $Rev$
+ * @Author      : 정재백
+ * @History     : 2024-03-09 최초 작성
+ * @Description : 테스트 케이스
+ **/
 package com.ntiple.commons;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class UtilsTestcase {
   @Test public void testSimple() {
-
+    assertTrue(true);
   }
 }
